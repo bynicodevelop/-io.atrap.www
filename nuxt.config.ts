ab8f9fe533aa931e76b5e6c69feabc1c6764da96
nico-develop <contact@nico-develop.fr>
2022-01-21 09:31:42 +0100

    Intégration de l'analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,8 @@ export default defineNuxtConfig({
       SITE_DESCRIPTION: process.env.SITE_DESCRIPTION,
       SITE_THUMBNAIL: process.env.SITE_THUMBNAIL,
       TWITTER_AUTHOR: process.env.TWITTER_AUTHOR,
+      GA4_ID: process.env.GA4_ID,
+      PIXEL_ID: process.env.PIXEL_ID,
     },
     privateRuntimeConfig: {
       NUXT_APP_MAILCHIMP_API_KEY: process.env.NUXT_APP_MAILCHIMP_API_KEY
